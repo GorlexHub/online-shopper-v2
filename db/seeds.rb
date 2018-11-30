498c69adef3bc4a8007486f8e4affbc7219af0d3
GorlexHub <alexandre.lovergne@gmail.com>
2018-11-30 11:18:30 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+niceCat = Product.create(title: "Quality Kitty", 
+ subtitle: "Learn App Code", image_name: "kitty.JPG",
+ price: "4.99", sku: "kittyOne", info: "cute cat", download_url: "http://image.noelshack.com/fichiers/2018/48/3/1543397663-chat1.jpeg", 
+ details: "A really nice cat", description: %{<p>It's a cat</p>
+ })
